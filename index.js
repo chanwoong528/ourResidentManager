ef61234d8ddd2d31ce19b7ec560736a54d6de2de
moon528 <cksdnd004@gmail.com>
2020-06-16 14:04:46 +0900

    first

---
+++ index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var app = express();
+var dbUrl = 'mongodb+srv://moon52892:ms5028@cluster0-ijllc.mongodb.net/lol-teamrank?retryWrites=true&w=majority';
+// DB setting
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(dbUrl);
+var db = mongoose.connection;
+db.once('open', function(){
+  console.log('DB connected');
+});
+db.on('error', function(err){
+  console.log('DB ERROR : ', err);
+});
+
+// Other settings
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+
+// Routes
+app.use('/', require('./routes/home'));
+app.use('/posts', require('./routes/posts'));
+app.use('/users', require('./routes/users'));
+
+// Port setting
+var port = 3000;
+app.listen(port, function(){
+  console.log('server on! http://localhost:'+port);
+});
