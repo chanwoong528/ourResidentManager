d0a56d1081b1cd3fa431ced47fbb5b7e71811c8c
moon528 <cksdnd004@gmail.com>
2020-07-16 01:24:53 +0900

    rendering the img if you upload one

--- routes/files.js
+++ routes/files.js
@@ -9,9 +9,10 @@ router.get('/:serverFileName/:originalFileName', function(req, res){ // 3
     var stream = file.getFileStream(); // 3-2
     if(stream){ // 3-3
       res.writeHead(200, {
-        'Content-Type': 'application/octet-stream',
-        'Content-Disposition': 'attachment; filename=' + file.originalFileName
+        'Content-Type': 'application/octet-stream; charset=utf-8',
+        'Content-Disposition': 'attachment; filename=' + encodeURI(file.originalFileName)
       });
+    
       stream.pipe(res);
     }
     else { // 3-4
