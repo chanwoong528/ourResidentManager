d661285633d888f21296fefbecd24bb6942a30c0
Dongkyun Kim <s98101000@gmail.com>
2020-07-16 14:19:17 +0900

    hotfix for server crash when sending korean in chat

--- libs/escapeUtil.js
+++ libs/escapeUtil.js
@@ -24,6 +24,8 @@ module.exports = {
   escapeNBSP: escapeNBSP
 }
 
+var hex=new Array('0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f');
+
 function escapeTags(char){
   var thechar = char.charCodeAt(0);
   switch (thechar) {
