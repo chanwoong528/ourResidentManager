e9646b208652767f1cbf8c2b4cb8097ca24e8056
moon528 <cksdnd004@gmail.com>
2020-07-18 19:00:43 +0900

    random genrator, encrpyt decrpyt finish

--- libs/keygen.js
+++ libs/keygen.js
@@ -1,11 +1,40 @@
+const crypto = require('crypto');
+
 /**
  * @returns randomly generated value to be used for client-server socket interactions.
  */
-module.exports.generateKey = function() {
-  var passKey = '';
-  // TODO: generate a passKey with random combination of numbers and letters?
-  // HOTP / TOTP
-  // May have to think about encryption later on.
+ var algorithm = 'aes-256-ctr';
+ var password = 'd6F3Efeq';
 
-  return passKey;
-}
+module.export.generateKey = function() {
+  var passKey = Math.random().toString(36).slice(2);
+            //  Math.random().toString(36)  -> "0.uk02kso845o"
+                //36 = 36진법: 숫자 0~9, 알파벳 -> "uk02kso845o"
+
+  return passKey;//stringkey return
+};
+
+
+module.export.encryptKey = function(key){
+
+  var cipher = crypto.createCipher(algorithm,password);
+  var crypted = cipher.update(key,'utf8','hex');
+   crypted += cipher.final('hex');
+   return crypted;
+};
+module.export.decryptKey = function(key){
+
+  var decipher = crypto.createDecipher(algorithm,password);
+  var dec = decipher.update(key,'hex','utf8');
+   dec += decipher.final('utf8');
+   return dec;
+};
+
+// var genkey = generateKey();
+//
+// console.log('orginal text:'+genkey);
+//
+// var encr = encryptKey(genkey);
+// console.log('encrypt:'+encr);
+//
+// console.log('decrypt:'+decryptKey(encr));
